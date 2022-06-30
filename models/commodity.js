b26316fd994556599a369315dcee26d9529b5ccf
DailyGrow <858519155@qq.com>
2022-06-29 22:25:03 -0700

    fix the problem in commodity schema

--- models/commodity.js
+++ models/commodity.js
@@ -6,7 +6,7 @@ var Schema = mongoose.Schema;
 
 var CommoditySchema=new Schema({
   supplier:{type: Schema.ObjectId, ref:'User', required:true},
-  commoidtyname:{type: String, required:true},
+  commodityname:{type: String, required:true},
   //email:{type:String, required:true},
   content:{type:String},
   //phone:{type:string, required:true},
